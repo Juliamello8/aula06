f97ded162715224c8003d20bdb1dee99d34726ff
Juliamello8 <juliainline@gmail.com>
2020-09-12 13:14:03 -0300

    Criando os atributos e métodos getters e setters

--- SistemaTarefa/src/com/github/juliamello8/classes/Tarefa.java
+++ SistemaTarefa/src/com/github/juliamello8/classes/Tarefa.java
@@ -1,5 +1,33 @@
 package com.github.juliamello8.classes;
 
-public class Tarefa {
-
+public class Tarefa extends Usuario{
+	private int id;
+	private String status;
+	private String titulo;
+	private String descricao;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
 }
