f0ea5d23993f2ac3e86b981bb975d042def0fd65
Juliamello8 <juliainline@gmail.com>
2020-09-12 14:33:50 -0300

    adicionando cadastro tarefa

--- ToDo/src/com/github/juliamello8/classes/Tarefa.java
+++ ToDo/src/com/github/juliamello8/classes/Tarefa.java
@@ -1,20 +1,23 @@
 package com.github.juliamello8.classes;
 
+import java.util.Scanner;
+
 public class Tarefa extends Usuario{
 	private int id;
-	private String status;
+	private char status;
 	private String titulo;
-	private String descricao;
+	private String descricao; 
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getStatus() {
+	public char getStatus() {
 		return status;
 	}
-	public void setStatus(String status) {
+	public void setStatus(char status) {
 		this.status = status;
 	}
 	public String getTitulo() {
@@ -29,5 +32,17 @@ public class Tarefa extends Usuario{
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+	public void CadastrarTarefa() {
+		try (Scanner leitor = new Scanner(System.in)) {			
+			System.out.println ("Título da tarefa: ");
+			titulo = leitor.nextLine(); 
+
+			System.out.println ("Descrição: ");
+			descricao = leitor.nextLine(); 
+
+			System.out.println ("Status: ");
+			status = leitor.next().charAt(0); 
+
+		} 
+	} 	
 }
